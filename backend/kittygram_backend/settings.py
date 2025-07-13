3fc7510b05af78e4c52098ec3b5318afe5a106c4
NChikurov <n.chikurov@gmail.com>
2025-07-13 10:15:26 +0300

    Fix media URL

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -106,6 +106,9 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+if not DEBUG:
+    MEDIA_URL = 'https://therealkittygram.webhop.me/media/'
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
